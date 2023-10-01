a9c344b17921482ab07b64f5ee8748608244091e
Mohamed <s2000588@edu.bc.fi>
2023-10-01 17:32:40 +0300

    complete all routes

--- backend/routes/Restaurant.routes.ts
+++ backend/routes/Restaurant.routes.ts
@@ -5,9 +5,12 @@ import * as RestaurantController from "../controllers/RestaurantController";
 const router = Router();
 
 router.get("/", RestaurantController.getRestaurants);
-
 router.get("/:id", RestaurantController.getRestaurant);
-
 router.post("/", RestaurantController.addRestaurant);
+router.put("/:id", RestaurantController.updateRestaurant);
+router.delete("/:id", RestaurantController.deleteRestaurant);
+router.post("/:id/addReview", RestaurantController.addReview);
+router.put("/:id/updateReview", RestaurantController.updateReview);
+router.delete("/:id/deleteReview", RestaurantController.deleteReview);
 
 export default router;
